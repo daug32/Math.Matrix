68d8f05a0a13875a6f4f054ed55ad51017210466
Dmitry Ivanov <daug32@mail.ru>
2023-02-10 03:02:39 +0300

    feat: Configured Core as a library

---
+++ Tests/Tests.cpp
@@ -0,0 +1,44 @@
+﻿#include "../Core/stdafx.h"
+#include <iostream>
+using namespace Math;
+
+void prtMatrix(Matrix a)
+{
+	for (auto& i : a.Buffer)
+	{
+		for (auto& j : i)
+		{
+			std::cout << j << ' ';
+		}
+
+		std::cout << std::endl;
+	}
+
+	std::cout << "=============\n";
+}
+
+int main()
+{
+	Matrix a(
+		{
+			{-1, -4, 0, 0, -2},
+			{0, 1, 1, 5, 4},
+			{3, 1, 7, 1, 0},
+			{0, 0, 2, 0, -3},
+			{-1, 0, 4, 2, 2}
+		});
+	prtMatrix(a);
+
+	auto b = a.Transponse();
+	prtMatrix(b);
+
+	std::cout << std::boolalpha << (b.Determinator() == a.Determinator()) << std::endl;
+
+	std::cout << std::boolalpha << (b != a) << std::endl;
+
+	b = b.Transponse();
+
+	std::cout << std::boolalpha << (b == a) << std::endl;
+
+	return 0;
+}
--- stdafx.h
+++
@@ -1,7 +0,0 @@
-#ifndef STDAFX_H
-#define STDAFX_H
-
-#include "Core/Math.h"
-#include <iostream>
-
-#endif
